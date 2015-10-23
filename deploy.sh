948e3c783509ffd94725a48b5af03d1cada36a86
Oleh Prypin <blaxpirit@gmail.com>
2015-10-23 21:41:00 +0300

    Add deployment to gh-pages with Travis CI

---
+++ deploy.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+set -o errexit -o nounset
+
+rev="$(git rev-parse --short HEAD)"
+
+git clone "$1" -b gh-pages gh-pages/
+
+cd gh-pages
+
+rm -rf tutorials
+mv ../tutorials .
+
+git config user.name 'Robot'
+git config user.email '<>'
+
+git add -A tutorials
+
+for f in tutorials/*.html; do
+    # See if there are any changes not related to "generator" or "revision"
+    (git diff --staged "$f" | tail -n +5 | grep -E '^[+\-]' |
+    grep -vE '<meta name="generator"|data-revision="') || unchanged+=("$f")
+done
+# Unstage files that changed only generation date
+[ -n "$unchanged" ] && git reset -- "${unchanged[@]}"
+
+git commit -m "Generate tutorials ($rev)"
+git push origin gh-pages >/dev/null 2>&1
