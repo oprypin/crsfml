6b7fab3793ba44c14756c583157d8fa69ee53481
Oleh Prypin <oleh@pryp.in>
2016-08-22 19:29:25 +0300

    Get rid of unsigned return values

--- generate.cr
+++ generate.cr
@@ -1141,6 +1141,9 @@ class CFunction < CItem
         if name == "SoundStream::Chunk"
           name = "Slice(Int16)"
         end
+        if param.type.full_name(Context::CPPSource) == "unsigned int"
+          name = "Int32" unless param.name(Context::Crystal).includes?("handle")
+        end
         "#{name}#{suffix}"
       }
       if return_params.size == 2 && return_params[0].type.full_name == "bool"
@@ -1346,6 +1349,8 @@ class CFunction < CItem
             "Array.new(#{name}_size.to_i) { |i| String.new(#{name}[i]) }"
           elsif typ =~ /\bstd::vector<(.+)>/
             "Array.new(#{name}_size.to_i) { |i| #{name}.as(#{$~[1]}*)[i] }"
+          elsif typ == "unsigned int"
+            "#{name}.to_i"
           elsif param.type.type.as?(CClass).try &.class? && const_reference_getter?
             "#{param.type.full_name(Context::Crystal)}::Reference.new(#{name}, self)"
           elsif param.type.type.is_a? CEnum
