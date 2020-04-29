6b6c0eb54f0f39d042f1cbb1a52d10d3c264a1ac
Oleh Prypin <oleh@pryp.in>
2020-04-30 01:25:37 +0200

    Hold on, I don't need Python, just use ECR

---
+++ .github/workflows/generate.cr
@@ -0,0 +1,13 @@
+require "ecr"
+require "yaml"
+
+outp = IO::Memory.new
+outp = ECR.embed("#{__DIR__}/ci.yml.ecr", outp)
+outp = outp.to_s
+outp = outp.gsub(/ *\\\n/, "").gsub(/( *\n)+/, "\n").gsub(/(?<=\S) +/, " ")
+
+File.write("#{__DIR__}/ci.yml.new", outp)
+
+YAML.parse(outp)
+
+File.rename("#{__DIR__}/ci.yml.new", "#{__DIR__}/ci.yml")
